cb26b2f72496c020cbd9800aa5a3cf88d443158a
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2021-08-09 02:55:49 +0700

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import "@/assets/global.scss";
 
 const el = document.createElement("div");
 document.body.appendChild(el);
